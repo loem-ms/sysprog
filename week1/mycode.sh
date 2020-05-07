6dbfa7108a56ee401849872485dd4b768f287e72
loem-ms <loem.m.aa@m.titech.ac.jp>
2020-05-07 12:43:02 +0000

    week1Quiz_finalversion

---
+++ week1/mycode.sh
@@ -0,0 +1,20 @@
+# 課題１
+echo /usr/include/u*d.h
+
+# 課題２
+grep '# define errno' /usr/include/*.h
+
+# 課題３
+grep '/tcp' services.txt | grep "^[^#;]" | wc -l
+
+# 課題４
+cut -f 1 syscall_headers.txt | sort | uniq | wc -l
+
+# 練習
+kill $(ps|grep sleep|grep -v 'grep sleep'|awk '{print $1}')
+
+# 課題５
+cut -f 2 syscall_headers.txt|sort|uniq -c|sort -n -r|head -n 3
+
+# 発展課題A
+grep -Fxf <(rev words.txt)  words.txt |wc -l
