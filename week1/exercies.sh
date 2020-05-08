8feb1dae27af618db4561894356c2bb44b9cd713
loem-ms <loem.m.aa@m.titech.ac.jp>
2020-05-08 06:20:49 +0000

    submitted exercises

--- week1/exercies.sh
+++ week1/exercies.sh
@@ -1,10 +1,12 @@
 # 課題１
-echo /usr/include/u*d.h
+echo u*d.h
 
 # 課題２
-grep '# define errno' /usr/include/*.h
+grep '# define errno' *.h
 
 # 課題３
+grep /tcp services.txt | grep -v '^#' | wc -l
+# or
 grep '/tcp' services.txt | grep "^[^#;]" | wc -l
 
 # 課題４
